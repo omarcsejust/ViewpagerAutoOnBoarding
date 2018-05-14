b3bcd764106efdae4739a26d878a0bae474f6dc4
omarcsejust <omarhasan951@gmail.com>
2018-05-14 00:56:41 -0700

    pusing to github

---
+++ app/src/main/java/com/example/omar/viewpagerautoonboarding/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.omar.viewpagerautoonboarding;
+
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eftimoff.viewpagertransformers.CubeInTransformer;
+import com.eftimoff.viewpagertransformers.CubeOutTransformer;
+import com.eftimoff.viewpagertransformers.ForegroundToBackgroundTransformer;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    int currentPage = 0;
+    Timer timer;
+    final long DELAY_MS = 800;//delay in milliseconds before task is to be executed
+    final long PERIOD_MS = 3000; // time in milliseconds between successive task executions.
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        CustomPagerAdapter customPagerAdapter=new CustomPagerAdapter(this);
+        viewPager.setAdapter(customPagerAdapter);
+        //viewPager.setAdapter(new CustomPagerAdapter(this));
+
+        viewPager.setPageTransformer(true,new CubeOutTransformer());
+
+        /*After setting the adapter use the timer */
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == 4-1) {
+                    currentPage = 0;
+                }
+                viewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
+        timer = new Timer(); // This will create a new Thread
+        timer .schedule(new TimerTask() { // task to be scheduled
+
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, DELAY_MS, PERIOD_MS);
+
+    }
+
+}
